26155ef38d744082823d945146caa0b72f7c1d37
Cory Latschkowski <koree@redhat.com>
2023-01-10 17:10:38 -0600

    update: sagemaker scripts
    Former-commit-id: 7fec54c874cc620ac5ed0513d7f0a0ea28f1cc8c

--- sagemaker/setup-k8s-tools.sh
+++ sagemaker/setup-k8s-tools.sh
@@ -9,7 +9,7 @@ setup_oc(){
 OCP_VERSION=4.10
 DOWNLOAD_URL=https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable-${OCP_VERSION}/openshift-client-linux.tar.gz
 
-curl "${DOWNLOAD_URL}" -L | tar vzx -C ${BIN_PATH}/ oc kubectl
+curl "${DOWNLOAD_URL}" -sL | tar vzx -C ${BIN_PATH}/ oc kubectl
 
 oc completion bash > ${COMPLETION_PATH}/oc.bash
 # . <(oc completion bash)
@@ -20,7 +20,7 @@ oc completion bash > ${COMPLETION_PATH}/oc.bash
 setup_helm(){
 DOWNLOAD_URL=https://mirror.openshift.com/pub/openshift-v4/clients/helm/latest/helm-linux-amd64.tar.gz
 
-curl "${DOWNLOAD_URL}" -L | tar vzx -C ${BIN_PATH}/ helm-linux-amd64
+curl "${DOWNLOAD_URL}" -sL | tar vzx -C ${BIN_PATH}/ helm-linux-amd64
 mv  ${BIN_PATH}/helm-linux-amd64  ${BIN_PATH}/helm
 
 helm completion bash > ${COMPLETION_PATH}/helm.bash
@@ -30,7 +30,7 @@ helm completion bash > ${COMPLETION_PATH}/helm.bash
 setup_s2i(){
 DOWNLOAD_URL=https://github.com/openshift/source-to-image/releases/download/v1.3.2/source-to-image-v1.3.2-78363eee-linux-amd64.tar.gz
 
-curl "${DOWNLOAD_URL}" -L | tar vzx -C ${BIN_PATH}/
+curl "${DOWNLOAD_URL}" -sL | tar vzx -C ${BIN_PATH}/
 
 s2i completion bash > ${COMPLETION_PATH}/s2i.bash
 # . <(s2i completion bash)
@@ -40,7 +40,7 @@ s2i completion bash > ${COMPLETION_PATH}/s2i.bash
 setup_restic(){
 DOWNLOAD_URL=https://github.com/restic/restic/releases/download/v0.14.0/restic_0.14.0_linux_amd64.bz2
 
-curl "${DOWNLOAD_URL}" -L | bzcat > ${BIN_PATH}/restic
+curl "${DOWNLOAD_URL}" -sL | bzcat > ${BIN_PATH}/restic
 chmod 755 ${BIN_PATH}/restic
 
 restic generate --bash-completion ${COMPLETION_PATH}/restic.bash
