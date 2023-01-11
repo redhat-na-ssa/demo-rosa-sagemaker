a10f2b732807159c079df0730179c53f5fde28e2
Cory Latschkowski <koree@redhat.com>
2023-01-11 14:09:51 -0600

    update: k8s setup tools
    Former-commit-id: 2e0e2f076f558519320d582b7b6a693d8cf9a3f9

--- sagemaker/setup-k8s-tools.sh
+++ sagemaker/setup-k8s-tools.sh
@@ -5,71 +5,91 @@ COMPLETION_PATH=/etc/bash_completion.d
 
 export PATH=${BIN_PATH}:${PATH}
 
-echo ${PATH}
+echo "${PATH}"
 
-setup_oc(){
-OCP_VERSION=4.10
-DOWNLOAD_URL=https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable-${OCP_VERSION}/openshift-client-linux.tar.gz
-
-curl "${DOWNLOAD_URL}" -sL | tar zx -C ${BIN_PATH}/ oc kubectl
-
-oc completion bash > ${COMPLETION_PATH}/oc.bash
-# . <(oc completion bash)
-# . <(kubectl completion bash)
-
+setup_bin(){
+  mkdir -p ${BIN_PATH}/bin
+  echo "${PATH}" | grep -q "${BIN_PATH}/bin" || \
+    PATH=$(pwd)/${BIN_PATH}/bin:${PATH}
+    export PATH
 }
 
-setup_odo(){
-DOWNLOAD_URL=https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/odo/v3.5.0/odo-linux-amd64.tar.gz
-
-curl "${DOWNLOAD_URL}" -sL | tar zx -C ${BIN_PATH}/
-
-odo completion bash > ${COMPLETION_PATH}/odo.bash
-
-
+check_bin(){
+  name=$1
+  
+  which "${name}" || download_"${name}"
+ 
+  case ${name} in
+    helm|kustomize|oc|odo|openshift-install|s2i)
+      echo "auto-complete: . <(${name} completion bash)"
+      
+      # shellcheck source=/dev/null
+      . <(${name} completion bash)
+      ${name} completion bash > "${COMPLETION_PATH}/${name}.bash"
+      
+      ${name} version
+      ;;
+    restic)
+      restic generate --bash-completion ${COMPLETION_PATH}/restic.bash
+      ;;
+    *)
+      echo
+      ${name} --version
+      ;;
+  esac
+  sleep 2
 }
 
-
-setup_helm(){
-DOWNLOAD_URL=https://mirror.openshift.com/pub/openshift-v4/clients/helm/latest/helm-linux-amd64.tar.gz
-
+download_helm(){
+BIN_VERSION=latest
+DOWNLOAD_URL=https://mirror.openshift.com/pub/openshift-v4/clients/helm/${BIN_VERSION}/helm-linux-amd64.tar.gz
 curl "${DOWNLOAD_URL}" -sL | tar zx -C ${BIN_PATH}/ helm-linux-amd64
 mv  ${BIN_PATH}/helm-linux-amd64  ${BIN_PATH}/helm
-
-helm completion bash > ${COMPLETION_PATH}/helm.bash
-
 }
 
-setup_s2i(){
-DOWNLOAD_URL=https://github.com/openshift/source-to-image/releases/download/v1.3.2/source-to-image-v1.3.2-78363eee-linux-amd64.tar.gz
+download_kustomize(){
+  cd "${BIN_PATH}" || return
+  curl -sL "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+  cd ../..
+}
 
+download_oc(){
+BIN_VERSION=4.10
+DOWNLOAD_URL=https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable-${BIN_VERSION}/openshift-client-linux.tar.gz
+curl "${DOWNLOAD_URL}" -sL | tar zx -C ${BIN_PATH}/ oc kubectl
+}
+
+download_odo(){
+BIN_VERSION=latest
+DOWNLOAD_URL=https://mirror.openshift.com/pub/openshift-v4/clients/odo/${BIN_VERSION}/odo-linux-amd64.tar.gz
 curl "${DOWNLOAD_URL}" -sL | tar zx -C ${BIN_PATH}/
-
-s2i completion bash > ${COMPLETION_PATH}/s2i.bash
-# . <(s2i completion bash)
-
 }
 
-setup_restic(){
-DOWNLOAD_URL=https://github.com/restic/restic/releases/download/v0.14.0/restic_0.14.0_linux_amd64.bz2
+download_s2i(){
+# BIN_VERSION=
+DOWNLOAD_URL=https://github.com/openshift/source-to-image/releases/download/v1.3.2/source-to-image-v1.3.2-78363eee-linux-amd64.tar.gz
+curl "${DOWNLOAD_URL}" -sL | tar zx -C ${BIN_PATH}/
+}
 
+download_restic(){
+BIN_VERSION=0.14.0
+DOWNLOAD_URL=https://github.com/restic/restic/releases/download/v${BIN_VERSION}/restic_${BIN_VERSION}_linux_amd64.bz2
 curl "${DOWNLOAD_URL}" -sL | bzcat > ${BIN_PATH}/restic
 chmod 755 ${BIN_PATH}/restic
-
-restic generate --bash-completion ${COMPLETION_PATH}/restic.bash
-
 }
 
-fix_perms(){
-
-chown ec2-user:ec2-user -R ${BIN_PATH} 
-
+kludge_sm_perms(){
+chown ec2-user:ec2-user -R ${BIN_PATH}
 }
 
-setup_oc
-setup_s2i
-setup_odo
+setup_bin
 
-setup_helm
-setup_restic
-fix_perms
+check_bin oc
+check_bin odo
+check_bin s2i
+
+check_bin helm
+check_bin kustomize
+check_bin restic
+
+kludge_sm_perms
