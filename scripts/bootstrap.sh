c6fc76d21d2a6935ec07d8c3399a7fe9e343706b
Cory Latschkowski <koree@redhat.com>
2023-01-16 13:43:25 -0600

    update: script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,6 +3,12 @@
 
 NAMESPACE=fingerprint-id
 
+get_aws_key(){
+  # get aws creds
+  export AWS_ACCESS_KEY_ID=$(oc -n kube-system extract secret/aws-creds --keys=aws_access_key_id --to=-)
+  export AWS_SECRET_ACCESS_KEY=$(oc -n kube-system extract secret/aws-creds --keys=aws_secret_access_key --to=-)
+}
+
 setup_namespace(){
   oc create ns "${NAMESPACE}" || return 0
   sleep 3
@@ -28,6 +34,8 @@ setup_odh(){
   oc -n "${NAMESPACE}" \
     apply -f openshift/sagemaker-notebook
 }
+
+get_aws_key
 
 setup_namespace
 setup_aws_crs
