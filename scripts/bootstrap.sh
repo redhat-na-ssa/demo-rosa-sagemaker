f1132cf7cdac1c19024f6d7b56201b98422676ff
Cory Latschkowski <koree@redhat.com>
2023-01-17 14:06:24 -0600

    add: trition automation

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -14,9 +14,13 @@ setup_namespace(){
   sleep 3
 }
 
-setup_aws_crs(){
+setup_sagemaker(){
   oc -n "${NAMESPACE}" \
     apply -f openshift/ack-examples
+  
+  aws iam create-role \
+    --policy-name AmazonSageMaker-ExecutionRole \
+    --policy-document file://sagemaker/awsexecutionrole-sagemaker.json
 }
 
 setup_odh(){
@@ -35,8 +39,36 @@ setup_odh(){
     apply -f openshift/sagemaker-notebook
 }
 
+setup_s2i_triton(){
+  NAMESPACE=example-triton
+
+  oc new-project ${NAMESPACE}
+
+  oc -n ${NAMESPACE} new-build \
+    https://github.com/codekow/s2i-patch.git \
+    --name s2i-triton \
+    --context-dir /s2i-triton \
+    --strategy docker
+  
+  export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-2}
+  
+  APP_NAME=model-server-s3
+
+  oc -n ${NAMESPACE} new-app \
+    s2i-triton:latest \
+    --name ${APP_NAME}
+
+  oc -n ${NAMESPACE} set env \
+    deploy/${APP_NAME} \
+    AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} \
+    AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+    AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+    MODEL_REPOSITORY=s3://sagemaker-fingerprint-models/models    
+}
+
 get_aws_key
 
 setup_namespace
 setup_aws_crs
 setup_odh
+setup_s2i_triton
