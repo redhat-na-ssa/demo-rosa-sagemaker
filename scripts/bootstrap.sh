9b0063b0fa84a3c063a06e32e83d7eb8c2e596fa
dmarcus-wire <dmarcus@redhat.com>
2023-02-07 14:05:30 -0700

    update us-east-2 in bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -54,7 +54,7 @@ get_aws_key(){
   # get aws creds
   export AWS_ACCESS_KEY_ID=$(oc -n kube-system extract secret/aws-creds --keys=aws_access_key_id --to=-)
   export AWS_SECRET_ACCESS_KEY=$(oc -n kube-system extract secret/aws-creds --keys=aws_secret_access_key --to=-)
-  export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-west-2}
+  export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-2}
 }
 
 setup_namespace(){
