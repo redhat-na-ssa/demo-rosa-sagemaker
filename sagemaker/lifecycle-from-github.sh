f81f7ab24d13bbb6fe37387a08614c2558db5a3b
Cory Latschkowski <koree@redhat.com>
2023-10-24 17:07:11 -0500

    update: lifecycle

--- sagemaker/lifecycle-from-github.sh
+++ sagemaker/lifecycle-from-github.sh
@@ -4,4 +4,4 @@
 id
 
 # run from repo
-curl -sL https://raw.githubusercontent.com/redhat-na-ssa/demo-rosa-sagemaker/alignment/sagemaker/lifecycle-on-start.sh | bash
+curl -sL https://raw.githubusercontent.com/redhat-na-ssa/demo-rosa-sagemaker/main/sagemaker/lifecycle-on-start.sh | bash
